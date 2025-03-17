02d6f0a9869cb584a0629c41f94c99e0c0db9790
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2025-03-17 16:29:49 -0400

    cleanup

--- CheckingApp/src/browser/CheckingApp-contribution.ts
+++ CheckingApp/src/browser/CheckingApp-contribution.ts
@@ -60,57 +60,6 @@ export class CheckingAppContribution implements FrontendApplicationContribution 
         await this.applicationShell.pendingUpdates;
     }
 
-    protected getDetailedOpenEditors(): Array<{
-        uri: string,
-        title: string,
-        isDirty: boolean,
-        isActive: boolean
-    }> {
-        const activeEditor = this.editorManager.activeEditor;
-
-        return this.editorManager.all.map(editor => ({
-            // @ts-ignore
-            uri: editor.uri.toString(),
-            title: editor.title.label,
-            // @ts-ignore
-            isDirty: editor.dirty,
-            isActive: editor === activeEditor
-        }));
-    }
-
-    protected setupEditorTracking(): void {
-        // Listen for active widget changes in the shell
-        this.toDispose.push(
-            this.applicationShell.onDidChangeActiveWidget(async () => {
-                const activeEditor = this.editorManager.activeEditor;
-                if (activeEditor) {
-                    console.log('Active editor changed:', activeEditor.title.label);
-                } else {
-                    console.log('No active editor');
-                }
-            })
-        );
-
-        // Listen for all widgets added or removed in the shell
-        this.toDispose.push(
-            this.applicationShell.onDidAddWidget(() => {
-                const openEditors = this.getDetailedOpenEditors();
-                console.log('onDidAddWidget - Updated list of open editors:', openEditors);
-            })
-        );
-        this.toDispose.push(
-            this.applicationShell.onDidRemoveWidget(() => {
-                const openEditors = this.getDetailedOpenEditors();
-                console.log('onDidRemoveWidget - Updated list of open editors:', openEditors);
-            })
-        );
-    }
-
-    // Add cleanup when the contribution is stopped
-    onStop(): void {
-        this.toDispose.dispose();
-    }
-
     /**
      * Initializes the layout when the application starts.
      * This method is invoked after the application shell is ready.
@@ -121,9 +70,6 @@ export class CheckingAppContribution implements FrontendApplicationContribution 
      */
     async onDidInitializeLayout(): Promise<void> {
         console.log('CheckingAppContribution.onDidInitializeLayout()');
-
-        // Setup editor tracking
-        // this.setupEditorTracking(); // TODO this breaks editor startup
         
         // Wait for the shell to be ready
         await this.applicationShell.pendingUpdates;
